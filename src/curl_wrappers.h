e5c8dade244778ada55cf793f1368dd0dde84f75
valdebrutal <carlosvalde9@gmail.com>
2025-09-12 09:53:00 +0300

    imporve token cache refresh and test access denied reason

--- src/curl_wrappers.h
+++ src/curl_wrappers.h
@@ -5,22 +5,22 @@
 #define CURL_WRAPPERS_H
 
 struct response_buffer {
-  char *buffer;
-  size_t size;
+    char* buffer;
+    size_t size;
 };
 
-struct response_buffer *new_response_buffer();
+struct response_buffer* new_response_buffer();
 
-void free_response_buffer(struct response_buffer *resp);
+void free_response_buffer(struct response_buffer* resp);
 
-long *get_response_status(CURL *client);
+long* get_response_status(CURL* client);
 
 enum http_status_code {
-  HTTP_OK = 200,
-  HTTP_BAD_REQUEST = 400,
-  HTTP_UNAUTHORIZED = 401,
-  HTTP_FORBIDDEN = 403,
-  HTTP_NOT_FOUND = 404
+    HTTP_OK = 200,
+    HTTP_BAD_REQUEST = 400,
+    HTTP_UNAUTHORIZED = 401,
+    HTTP_FORBIDDEN = 403,
+    HTTP_NOT_FOUND = 404
 };
 
 enum http_status_code http_status_code_from(long code);
